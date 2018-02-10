ffb054010d78ac54c8113f41aa055add452aded7
Anshul Mittal <anshulmittal0409@gmail.com>
2018-02-10 19:20:07 +0530

    Add hospital-list-item component
    Signed-off-by: Anshul Mittal <anshulmittal0409@gmail.com>

---
+++ src/app/hospital-list-item/hospital-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hospital-list-item',
+  templateUrl: './hospital-list-item.component.html',
+  styleUrls: ['./hospital-list-item.component.css']
+})
+export class HospitalListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
