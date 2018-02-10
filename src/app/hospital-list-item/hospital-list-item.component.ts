c24186a01498169d01a653821a5753bc4ee2c72b
Anshul Mittal <anshulmittal0409@gmail.com>
2018-02-10 20:08:55 +0530

    Create Hospital-list-item
    Signed-off-by: Anshul Mittal <anshulmittal0409@gmail.com>

--- src/app/hospital-list-item/hospital-list-item.component.ts
+++ src/app/hospital-list-item/hospital-list-item.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hospital-list-item.component.css']
 })
 export class HospitalListItemComponent implements OnInit {
+  selectedSchedule: any;
 
-  constructor() { }
+  setSchedule: any = {
+    Mon: ['10:00', '10:15', '10:30', '10:45', '11:00'],
+    Tue: ['10:30', '10:45', '11:00', '11:15', '11:30'],
+    Wed: ['11:30', '11:45', '12:00', '12:15', '12:30'],
+    Thurs: ['1:30', '1:45', '2:00', '2:15', '2:30'],
+    Fri: ['7:30', '7:45', '8:00', '8:15', '8:30'],
+    Sat: ['8:30', '8:45', '9:00', '9:15', '9:30']
+  };
 
-  ngOnInit() {
+  onSelect(day: string): void {
+    this.selectedSchedule = this.setSchedule[day];
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
