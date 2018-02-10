516d0f8a8ee295888e5d8232e9f5ec71b046dc2a
devanshjain98 <devanshjain98@yahoo.in>
2018-02-11 01:45:35 +0530

    made health data

---
+++ src/app/health-data/health-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-health-data',
+  templateUrl: './health-data.component.html',
+  styleUrls: ['./health-data.component.css']
+})
+export class HealthDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
