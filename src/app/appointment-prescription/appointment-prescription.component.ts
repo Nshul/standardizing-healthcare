b9bbf8812a012cf87150503d2ff44832c97497af
Anshul Mittal <anshulmittal0409@gmail.com>
2018-02-11 01:27:37 +0530

    Integrate appointment-prescription.component.html with appointment-prescription.component.ts
    Signed-off-by: Anshul Mittal <anshulmittal0409@gmail.com>

--- src/app/appointment-prescription/appointment-prescription.component.ts
+++ src/app/appointment-prescription/appointment-prescription.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./appointment-prescription.component.css']
 })
 export class AppointmentPrescriptionComponent implements OnInit {
+  hospitalName: string = 'GoofedUp Hospital';
+  hospitalAddress: string = ' DLF Phase-5, Meerut ';
+  prescriptionDate: string = '21-04-2018';
+  prescribedMedicine: MedicineList[] = [
+    {
+      medicine: 'Medicine1',
+      dosage: '2 times a day',
+      comment: 'take for 3 days'
+    },
+    {
+      medicine: 'Medicine2',
+      dosage: '3 times a day',
+      comment: 'take for 2 days'
+    },
+    {
+      medicine: 'Medicine3',
+      dosage: '1 times a day',
+      comment: 'take for 2 days'
+    },
+    {
+      medicine: 'Medicine4',
+      dosage: '2 times a day',
+      comment: 'take for 6 days'
+    }
+  ];
+  doctorName: string = 'Mr Goofy Patil';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+}
 
+class MedicineList {
+  medicine: string;
+  dosage: string;
+  comment: string;
 }
