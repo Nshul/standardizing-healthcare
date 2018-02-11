bb3a1e5cef7fc12555c4dd25fc86545433e607f3
Anshul Mittal <anshulmittal0409@gmail.com>
2018-02-11 10:26:44 +0530

    Accomodate the appointments for services
    Signed-off-by: Anshul Mittal <anshulmittal0409@gmail.com>

--- src/app/auth/hospitalsignup.service.ts
+++ src/app/auth/hospitalsignup.service.ts
@@ -1,109 +1,134 @@
 import { Injectable } from '@angular/core';
-import {Subject} from 'rxjs';
+import { Subject } from 'rxjs';
 import * as firebase from 'firebase';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Injectable()
 export class HospitalsignupService {
-
-	token : string;
+  token: string;
   errormessage = new Subject<string>();
 
-  constructor(private router : Router, private activatedRoute : ActivatedRoute) { }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
-  signup(email,pass,name,contact,address,pincode,services,tier,geolocation){
-      firebase.auth().createUserWithEmailAndPassword(email,pass)
-      .then((hospital) =>{
+  signup(
+    email,
+    pass,
+    name,
+    contact,
+    address,
+    pincode,
+    services,
+    tier,
+    geolocation
+  ) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, pass)
+      .then(hospital => {
         console.log(hospital);
-        firebase.database().ref(`/hospitals/${hospital.uid}`)
-        .set({ email,pass,name,contact,address,pincode,services,tier});
+        firebase
+          .database()
+          .ref(`/hospitals/${hospital.uid}`)
+          .set({
+            email,
+            pass,
+            name,
+            contact,
+            address,
+            pincode,
+            services,
+            tier
+          });
         // .then(() => { console.log("done"); }).catch( err => { console.log(err); });
         var price: number;
-        if(tier=='gold'){
+        if (tier == 'gold') {
           price = 1000;
-        }else if(tier=='platinum'){
+        } else if (tier == 'platinum') {
           price = 1500;
-        }else{
+        } else {
           price = 500;
         }
         var rating: number = 4;
         var schedule = {
-          "fri":{
-            time_start: "12:00",
-            time_end: "13:00"
+          fri: {
+            time_start: '12:00',
+            time_end: '13:00'
           },
-          "mon":{
-            time_start: "07:00",
-            time_end: "13:00"
+          mon: {
+            time_start: '07:00',
+            time_end: '13:00'
           },
-          "sat":{
-            time_start: "09:00",
-            time_end: "12:00"
+          sat: {
+            time_start: '09:00',
+            time_end: '12:00'
           },
-          "thurs":{
-            time_start: "11:00",
-            time_end: "12:00"
+          thurs: {
+            time_start: '11:00',
+            time_end: '12:00'
           },
-          "tue":{
-            time_start: "08:00",
-            time_end: "16:00"
+          tue: {
+            time_start: '08:00',
+            time_end: '16:00'
           },
-          "wed":{
-            time_start: "06:00",
-            time_end: "09:00"
+          wed: {
+            time_start: '06:00',
+            time_end: '09:00'
           }
+        };
+        for (let service of services) {
+          firebase
+            .database()
+            .ref(`/services/${service}/${pincode}/${hospital.uid}`)
+            .set({ address, geolocation, name, price, rating, schedule });
         }
-        firebase.database().ref(`/services/${pincode}/${hospital.uid}`)
-        .set({address, geolocation,name,price,rating,schedule})
-
-        this.router.navigate(['../hospitallanding'],{relativeTo : this.activatedRoute});
-
+        this.router.navigate(['../hospitallanding'], {
+          relativeTo: this.activatedRoute
+        });
       })
-      .catch(
-      	error => { console.log(error); this.errormessage.next(error.message); }
-      	);
-
+      .catch(error => {
+        console.log(error);
+        this.errormessage.next(error.message);
+      });
   }
 
-   signin(email : string, pass: string){
-      firebase.auth().signInWithEmailAndPassword(email,pass)
-      .then(
-      	(response) => {
-      		this.router.navigate(['../hospitallanding'],{relativeTo : this.activatedRoute});
-      		console.log(response)
-            firebase.auth().currentUser.getToken()
-            .then(
-              (token : string) => {
-               this.token = token;
-       }
-       );
-      	}
-      	)
-      .catch(
-      	(error) => { console.log(error); this.errormessage.next(error.message); }
-      	);
-      
+  signin(email: string, pass: string) {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, pass)
+      .then(response => {
+        this.router.navigate(['../hospitallanding'], {
+          relativeTo: this.activatedRoute
+        });
+        console.log(response);
+        firebase
+          .auth()
+          .currentUser.getToken()
+          .then((token: string) => {
+            this.token = token;
+          });
+      })
+      .catch(error => {
+        console.log(error);
+        this.errormessage.next(error.message);
+      });
   }
 
-  getToken(){
-  	firebase.auth().currentUser.getToken()
-            .then(
-              (token : string) => {
-               this.token = token;
-       }
-       );
-     return this.token ;
+  getToken() {
+    firebase
+      .auth()
+      .currentUser.getToken()
+      .then((token: string) => {
+        this.token = token;
+      });
+    return this.token;
   }
 
-  isAuth(){
-  	return this.token != null;
+  isAuth() {
+    return this.token != null;
   }
 
-  logout(){
-  	firebase.auth().signOut();
-  	this.token = null;
-    
-  	
+  logout() {
+    firebase.auth().signOut();
+    this.token = null;
   }
-
 }
--- src/app/hospital-landingpage/hospital-landingpage.component.ts
+++ src/app/hospital-landingpage/hospital-landingpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-hospital-landingpage',
@@ -6,36 +7,100 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hospital-landingpage.component.css']
 })
 export class HospitalLandingpageComponent implements OnInit {
-pendingAppointments: AppointmentList[] = [
+  hospitalUser: any;
+
+  pendingAppointments: AppointmentList[] = [
     {
-      appointment_id: '1',
-type: 'service',
-docid:'abc'
+      service_type: 'Test1 Service',
+      date: '78-78-1995',
+      timeslot: '11:15',
+      appointmentKey: 'aksjbdkasjdnkads'
     },
     {
-      appointment_id: '2',
-type: 'service',
-docid:'ac'
-    },
-    {
-      appointment_id: '3',
-type: 'service',
-docid:'bc'
-    },
-    {
-      appointment_id: '4',
-type: 'service',
-docid:'abdc'
+      service_type: 'Test2 Service',
+      date: '14-05-1245',
+      timeslot: '12:45',
+      appointmentKey: '324ioewj234sa'
     }
+    // {
+    //   appointment_id: '3',
+    //   type: 'service',
+    //   docid: 'bc'
+    // },
+    // {
+    //   appointment_id: '4',
+    //   type: 'service',
+    //   docid: 'abdc'
+    // }
   ];
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    const UID = firebase.auth().currentUser.uid;
+    firebase
+      .database()
+      .ref(`hospitals/${UID}`)
+      .on('value', snapshot => {
+        this.hospitalUser = snapshot.val();
+      });
+
+    firebase
+      .database()
+      .ref(`appointments/${UID}`)
+      .on('value', snapshot => {
+        const temp1 = snapshot.val();
+        console.log('temp1');
+        console.log(temp1);
+        for (let i in temp1) {
+          for (let j in temp1[i]) {
+            console.log('j:');
+            console.log(j);
+            const service_type = i;
+            const date = j;
+            const timeslot;
+            const appointmentKey;
+            for (let k in temp1[i][j].requested) {
+              console.log('k:');
+              console.log(k);
+              timeslot = temp1[i][j].requested[k].timeslot;
+              appointmentKey = k;
+            }
+            if (temp1[i][j].requested[k].status == '-1') {
+              this.pendingAppointments.push({
+                service_type,
+                date,
+                timeslot,
+                appointmentKey
+              });
+            }
+          }
+        }
+      });
+  }
+
+  approveAppointment(serviceType: string, date: string, key: string) {
+    const UID = firebase.auth().currentUser.uid;
+    firebase
+      .database()
+      .ref(`appointments/${UID}/${serviceType}/${date}/requested/${key}`)
+      .update({
+        status: '1'
+      });
+  }
+
+  rejectAppointment(serviceType: string, date: string, key: string) {
+    const UID = firebase.auth().currentUser.uid;
+    firebase
+      .database()
+      .ref(`appointments/${UID}/${serviceType}/${date}/requested/${key}`)
+      .update({
+        status: '0'
+      });
   }
 }
 class AppointmentList {
-appointment_id: string;
-type: string;
-docid:string;
+  service_type: string;
+  date: string;
+  timeslot: string;
+  appointmentKey: string;
 }
-
